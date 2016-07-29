067d3bbbe66f21afd183fac589a747492dee5456
da7th - Moe <m.da7th@gmail.com>
2016-07-29 05:07:56 +0300

    added some java classes

---
+++ app/src/main/java/com/example/android/inventoryapp/ItemAdapter.java
@@ -0,0 +1,7 @@
+package com.example.android.inventoryapp;
+
+/**
+ * Created by da7th on 7/29/2016.
+ */
+public class ItemAdapter {
+}
--- app/src/main/java/com/example/android/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.android.inventoryapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    TextView productNameTV, priceTV, quantityTV;
+    Button addItemB;
+    private String LOG_TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
 }
