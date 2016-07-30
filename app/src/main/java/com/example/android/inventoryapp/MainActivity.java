b08f5bb81058d501bb794d39039efe818d92f27b
da7th - Moe <m.da7th@gmail.com>
2016-07-31 02:43:15 +0300

    checkpoint in work sold field working and incrementing 2

--- app/src/main/java/com/example/android/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/MainActivity.java
@@ -60,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Intent addItemIntent = new Intent(MainActivity.this, AddItemFragment2.class);
+                Intent addItemIntent = new Intent(MainActivity.this, AddItemFragment.class);
                 startActivity(addItemIntent);
 
             }
